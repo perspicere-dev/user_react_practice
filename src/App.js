9bd6a59519ebf155b234bfe6278784539dee7791
Przemysław Wnęk <wnek.przemek@gmail.com>
2022-02-09 19:04:25 +0100

    add modal - nice working on state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AddUser from "./components/User/AddUser";
+import UsersList from "./components/User/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const adduserhandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { id: Math.random().toString(), name: uName, age: uAge },
+      ];
+    });
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={adduserhandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
